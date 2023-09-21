059cee52747fa3916974558c72d13b04985b12f4
Michael Jonathan <michael.jonathan004@binus.ac.id>
2023-09-21 22:46:58 +0700

    bikin tabel

--- resources/views/Student/Index.blade.php
+++ resources/views/Student/Index.blade.php
@@ -5,13 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student Page</title>
+    <style>
+        .text-bold {
+            font-weight: bold;
+        }
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
     <h1 class="text-bold">Nama Mahasiswa</h1>
     <table>
         <thead>
-            <th>NIM</th>
-            <th>NAMA</th>
+            <tr>
+                <th>NIM</th>
+                <th>NAMA</th>
+            </tr>
         </thead>
         <tbody>
             @foreach($students as $item)
